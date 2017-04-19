93eba2653029645f0112b04c77469bc70b8cb88b
npappassl <npappas@scottlogic.com>
2017-04-19 16:23:50 +0100

    filled book repo

---
+++ library-grad-project/Repos/FilledBookRepository.cs
@@ -0,0 +1,23 @@
+﻿using LibraryGradProject.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace LibraryGradProject.Repos
+{
+    public class FilledBookRepository : BookRepository
+    {
+
+        public FilledBookRepository()
+        {
+            Add(new Book()
+            {
+                Title = "El pneumatico",
+                Author = "Nikos Pappas",
+                ISBN = "23123012093187",
+                PublishDate = "21/4/1986"
+            });
+
+        }
+    }
+}
