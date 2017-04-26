c0a2f0f508a174d9dadadbc1a6328b657c94fc72
npappassl <npappas@scottlogic.com>
2017-04-26 13:19:42 +0100

    added context factory file

---
+++ library-grad-project/Repos/DbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryGradProject.Repos
+{
+    public class DbContextFactory
+    {
+        public LibraryContext Create(string connectionString)
+        {
+            return new LibraryContext(connectionString);
+        }
+    }
+}
